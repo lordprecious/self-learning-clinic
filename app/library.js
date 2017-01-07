225545d832567babeb3bf26e82f4e05bbd289a5d
Andela Bootcamp <lordprecious1@gmail.com>
2017-01-07 10:36:00 +0100

    delete previous files and replace with new ones

---
+++ app/library.js
@@ -0,0 +1,61 @@
+'use strict'
+
+var file = {}
+
+file.fizzBuzz = function(num) {
+
+  if(num % 5 === 0 && num % 3 === 0){
+    return "FizzBuzz";
+  }else if(num % 5 === 0){
+    return "Buzz";
+  }
+  else if(num % 3 === 0){
+    return "Fizz";
+  }else{
+    return num;
+  }
+
+ }
+
+file.findMinMax = function(l) {
+
+
+    var output = [];
+  var same = l[0];
+  for(j = 1; j < 2; j++){
+    if(l[j] == same){
+    output[0] = l[j];
+    return output;
+    }
+  }
+  if(l.length < 2){
+    return l;
+  }else{
+    
+    output[0] = minimum(l);
+    output[1] = maximum(l);
+    return output;
+  }
+
+ }
+
+
+ function minimum(l){
+  var min = l[0];
+  for(i = 1; i <= l.length; i++){
+    if(l[i] < min){
+      min = l[i];
+    }
+  }
+  return min;
+}
+
+function maximum(l){
+  var max = l[0];
+  for(i = 1; i <= l.length; i++){
+    if(l[i] > max){
+      max = l[i];
+    }
+  }
+  return max;
+}
---
+++ tests/tests.js
@@ -0,0 +1,73 @@
+
+'use strict';
+
+var chai = require('jasmine');
+var expect = chai.expect;
+
+var myApp = require('../app/library.js');
+
+  describe('Min-Max Numbers in a List: ', function () {
+
+    describe('Return the min and max number in the list in a new list follows `[min, max]`', function () {
+
+      it('should return [1,4] for [1, 2, 3 , 4]', function () {
+        expect(findMinMax([1, 2, 3, 4])).toEqual([1, 4]);
+      });
+
+      it('should return [4, 6] for [6, 4]', function () {
+        expect(findMinMax([6, 4])).toEqual([4, 6]);
+      });
+
+      it('should return [2, 78] for [4, 66, 6, 44, 7, 78, 8, 68, 2]', function () {
+        expect(findMinMax([4, 66, 6, 44, 7, 78, 8, 68, 2])).toEqual([2, 78]);
+      });
+
+    });
+
+    describe('Return the number in the list in a new list follows `[min]` when the number is the min and max number in that list', function () {
+
+      it('should return [4] for [4, 4, 4, 4]', function () {
+        expect(findMinMax([4, 4, 4, 4])).toEqual([4]);
+      });
+
+    });
+
+  });
+
+
+
+describe("Fizz Buzz tests ", function() {
+
+  it("should return `Fizz` for number divisible by 3", function() {
+    expect(fizzBuzz(3)).toBe('Fizz');
+  });
+
+  it("should return `Buzz` for number divisible by 5", function() {
+    expect(fizzBuzz(5)).toBe('Buzz');
+  });
+
+  it("should return `FizzBuzz` for 15", function() {
+    expect(fizzBuzz(15)).toBe('FizzBuzz');
+  });
+
+  it("should return `FizzBuzz` for 45", function() {
+    expect(fizzBuzz(45)).toBe('FizzBuzz');
+  });
+
+  it("should return `FizzBuzz` for 90", function() {
+    expect(fizzBuzz(90)).toBe('FizzBuzz');
+  });
+
+  it("should return `Fizz` for 63", function() {
+    expect(fizzBuzz(63)).toBe('Fizz');
+  });
+
+  it("should return 7 since its indivisible by 3 and 5", function() {
+    expect(fizzBuzz(7)).toBe(7);
+  });
+
+  it("should return 101 since its indivisible by 3 and 5", function() {
+    expect(fizzBuzz(101)).toBe(101);
+  });
+
+});
