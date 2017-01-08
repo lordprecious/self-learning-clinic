c39a49948f4df7abab0e8fd74bca1bf5b83210c9
Andela Bootcamp <lordprecious1@gmail.com>
2017-01-08 20:24:44 +0100

    add funtion and more test cases

--- app/library.js
+++ app/library.js
@@ -60,4 +60,33 @@ function maximum(l){
   }
   return max;
 }
+
+file.aritGeo = function(arr) { 
+  
+  if(arr.length < 1) {
+    return 0;
+  }
+  
+  var diff = arr[1] - arr[0];
+  var ratio = arr[1] / arr[0];
+
+  var arith = true;
+  var geo = true;
+
+  for(var i = 0; i < arr.length - 1; i++) {
+    if( arr[i + 1] - arr[i] !== diff )
+        arith = false;
+    if(arr[i + 1] / ratio !== arr[i])
+        geo = false;
+  }
+
+  if(arith === true)
+    return "Arithmetic";
+  else if(geo === true)
+    return "Geometric";
+  else
+    return -1;
+
+}
+
 module.exports = file;
--- tests/tests.js
+++ tests/tests.js
@@ -79,3 +79,89 @@ describe("Fizz Buzz tests ", function() {
   });
 
 });
+
+
+describe("Determine the sequence of an array of numbers: ", function() {
+
+    describe("Case for an empty array", function() {
+
+      it("should return 0 for an empty array", function() {
+        expect(myApp.aritGeo([])).toEqual(0);
+      });
+
+    });
+
+    describe("Case for an arithmetic sequence", function() {
+
+      it("should return `Arithmetic` for [2, 4, 6, 8, 10]", function() {
+        expect(myApp.aritGeo([2, 4, 6, 8, 10])).toEqual('Arithmetic');
+      });
+
+      it("should return `Arithmetic` for [5, 11, 17, 23, 29, 35, 41]", function() {
+        expect(myApp.aritGeo([5, 11, 17, 23, 29, 35, 41])).toEqual('Arithmetic');
+      });
+
+      it("should return `Arithmetic` for [15, 10, 5, 0, -5, -10]", function() {
+        expect(myApp.aritGeo([15, 10, 5, 0, -5, -10])).toEqual('Arithmetic');
+      });
+
+      it("should return `Arithmetic` for [0, 10, 20, 30, 40, 50]", function() {
+        expect(myApp.aritGeo([0, 10, 20, 30, 40, 50])).toEqual('Arithmetic');
+      });
+
+      it("should return `Arithmetic` for [0, 100, 200, 300, 400, 500]", function() {
+        expect(myApp.aritGeo([0, 100, 200, 300, 400, 500])).toEqual('Arithmetic');
+      });
+
+      it("should return `Arithmetic` for [0, 1000, 2000, 3000, 4000, 5000]", function() {
+        expect(myApp.aritGeo([0, 1000, 2000, 3000, 4000, 5000])).toEqual('Arithmetic');
+      });
+
+      it("should return `Arithmetic` for [-100, -90, -80, -70, -60, -50]", function() {
+        expect(myApp.aritGeo([-100, -90, -80, -70, -60, -50])).toEqual('Arithmetic');
+      });
+
+
+
+    });
+
+    describe("Case for a geometric sequence", function() {
+
+      it("should return `Geometric` for [2, 6, 18, 54, 162]", function() {
+        expect(myApp.aritGeo([2, 6, 18, 54, 162])).toEqual('Geometric');
+      });
+
+      it("should return `Geometric` for [0.5, 3.5, 24.5, 171.5]", function() {
+        expect(myApp.aritGeo([0.5, 3.5, 24.5, 171.5])).toEqual('Geometric');
+      });
+
+      it("should return `Geometric` for [−128, 64, −32, 16, −8]", function() {
+        expect(myApp.aritGeo([-128, 64, -32, 16, -8])).toEqual('Geometric');
+      });
+
+    });
+
+    describe("Case for neither arithmetic nor geometric sequence", function() {
+
+      it("should return -1 for [1, 2, 3, 5, 8]", function() {
+        expect(myApp.aritGeo([1, 2, 3, 5, 8])).toEqual(-1);
+      });
+
+      it("should return -1 for [1, 3, 6, 10, 15]", function() {
+        expect(myApp.aritGeo([1, 3, 6, 10, 15])).toEqual(-1);
+      });
+
+      it("should return -1 for [1, 8, 27, 64, 125]", function() {
+        expect(myApp.aritGeo([1, 8, 27, 64, 125])).toEqual(-1);
+      });
+
+      it("should return -1 for [1, 3, 247, -64, 12]", function() {
+        expect(myApp.aritGeo([1, 3, 247, -64, 12])).toEqual(-1);
+      });
+
+      it("should return -1 for [10, 3, -47, 6764, -2]", function() {
+        expect(myApp.aritGeo([10, 3, -47, 6764, -2])).toEqual(-1);
+      });
+      
+    });
+  });
